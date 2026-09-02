ae6b87c1ac7122cdb7308120be673dcba3e97280
Tobias Frauenschläger <tobias@wolfssl.com>
2026-09-02 16:09:43 +0200

    wolfcrypt test: skip the ML-KEM ASN.1 tests without an allocator
    The ML-KEM ASN.1, certificate and certificate generation tests hold the key
    objects on the heap and size their DER buffers at run time, so they need an
    allocator. Guard them on !WOLFSSL_NO_MALLOC: the codec itself works in such
    a build, only the tests do not.

--- wolfcrypt/test/test.c
+++ wolfcrypt/test/test.c
@@ -62641,6 +62641,7 @@ out:
 #endif /* !WOLFSSL_NO_KYBER1024 && !WOLFSSL_NO_ML_KEM_1024 */
 
 #if !defined(WOLFSSL_MLKEM_NO_ASN1) && \
+    !defined(WOLFSSL_NO_MALLOC) && \
     defined(WC_ENABLE_ASYM_KEY_EXPORT) && \
     defined(WC_ENABLE_ASYM_KEY_IMPORT) && \
     !defined(WOLFSSL_MLKEM_NO_MAKE_KEY) && !defined(WC_NO_RNG) && \
@@ -63083,6 +63084,7 @@ free_level:
  * runs in WOLFSSL_MLKEM_NO_MAKE_KEY builds too, which is exactly what that key
  * format was chosen for. */
 #if !defined(WOLFSSL_MLKEM_NO_ASN1) && !defined(NO_FILESYSTEM) && \
+    !defined(WOLFSSL_NO_MALLOC) && \
     !defined(NO_ASN) && defined(WC_ENABLE_ASYM_KEY_IMPORT) && \
     defined(WC_ENABLE_ASYM_KEY_EXPORT)
 
@@ -63236,6 +63238,7 @@ free_vector:
  * ML-DSA because it is in far more builds, and the MLKEM_TYPE branches under
  * test do not depend on the issuer algorithm. */
 #if defined(WOLFSSL_CERT_GEN) && defined(WOLFSSL_CERT_EXT) && \
+    !defined(WOLFSSL_NO_MALLOC) && \
     defined(WOLFSSL_TEST_CERT) && !defined(WOLFSSL_MLKEM_NO_ASN1) && \
     !defined(WOLFSSL_MLKEM_NO_MAKE_KEY) && !defined(NO_ASN) && \
     defined(WC_ENABLE_ASYM_KEY_EXPORT) && defined(WC_ENABLE_ASYM_KEY_IMPORT) && \
@@ -63748,6 +63751,7 @@ WOLFSSL_TEST_SUBROUTINE wc_test_ret_t mlkem_test(void)
 #endif
 
 #if !defined(WOLFSSL_MLKEM_NO_ASN1) && \
+    !defined(WOLFSSL_NO_MALLOC) && \
     defined(WC_ENABLE_ASYM_KEY_EXPORT) && \
     defined(WC_ENABLE_ASYM_KEY_IMPORT) && \
     !defined(WOLFSSL_MLKEM_NO_MAKE_KEY) && !defined(WC_NO_RNG) && \
@@ -63762,6 +63766,7 @@ WOLFSSL_TEST_SUBROUTINE wc_test_ret_t mlkem_test(void)
 #endif
 
 #if !defined(WOLFSSL_MLKEM_NO_ASN1) && !defined(NO_FILESYSTEM) && \
+    !defined(WOLFSSL_NO_MALLOC) && \
     !defined(NO_ASN) && defined(WC_ENABLE_ASYM_KEY_IMPORT) && \
     defined(WC_ENABLE_ASYM_KEY_EXPORT)
     ret = mlkem_cert_test();
@@ -63770,6 +63775,7 @@ WOLFSSL_TEST_SUBROUTINE wc_test_ret_t mlkem_test(void)
 #endif
 
 #if defined(WOLFSSL_CERT_GEN) && defined(WOLFSSL_CERT_EXT) && \
+    !defined(WOLFSSL_NO_MALLOC) && \
     defined(WOLFSSL_TEST_CERT) && !defined(WOLFSSL_MLKEM_NO_ASN1) && \
     !defined(WOLFSSL_MLKEM_NO_MAKE_KEY) && !defined(NO_ASN) && \
     defined(WC_ENABLE_ASYM_KEY_EXPORT) && defined(WC_ENABLE_ASYM_KEY_IMPORT) && \
